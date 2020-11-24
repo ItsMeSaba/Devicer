da2245b521416f741339cdca2dcd94ef826d58e2
saba <sbsilagadze@gmail.com>
2020-11-24 14:25:13 +0400

    corrections

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import ReactGA from 'react-ga';
 
-ReactGA.initialize('168546471'); // add your tracking id here.
+ReactGA.initialize('253759624'); // add your tracking id here.
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
